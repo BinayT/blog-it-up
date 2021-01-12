37d90759b8e535f65bf001e53fed8499a371b9bb
Binay <binayprogramming@gmail.com>
2021-01-12 15:56:58 +0100

    Bcrypt and jwt installed to hash and hand out tokens

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import User from '../models/userModel.js';
 import { body, validationResult } from 'express-validator';
 
 const registrationValidations = [
-  body('email').isEmail().withMessage('Valid email address is required'),
+  body('email').isEmail().trim().withMessage('Valid email address is required'),
   body('password')
     .isLength({ min: 4 })
     .withMessage('Password must be of at least 4 characters'),
@@ -11,13 +11,22 @@ const registrationValidations = [
 //@Desc   Create new User
 //@route  POST /register
 //@access Public
-const userRegister = (req, res) => {
+const userRegister = async (req, res) => {
+  const { email, password, name } = req.body;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  const { email, password, name } = req.body;
-  res.json({ email, password, name });
+
+  try {
+    const checkUser = await User.findOne({ email });
+
+    checkUser &&
+      res.status(400).json({ errors: [{ msg: 'Email already in use.' }] });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
 };
 
 export { userRegister, registrationValidations };
