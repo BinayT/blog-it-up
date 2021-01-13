8e864d7dbdf1829cc56411568ee02c59064411f0
Binay <binayprogramming@gmail.com>
2021-01-13 16:05:03 +0100

    SASS file structure completed, main.scss imported in app.js and working perfectly

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './Screens/Home';
+import Register from './Screens/Register';
+import Login from './Screens/Login';
+import './main.scss';
 
 function App() {
   return (
     <Switch>
-      <Route path='/' component={Home} />
+      <Route path='/' exact component={Home} />
+      <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
     </Switch>
   );
 }
