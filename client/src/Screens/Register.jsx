f4f45913c59e361aec9986c05b6c214fb245c9d3
Binay <binayprogramming@gmail.com>
2021-01-14 15:08:05 +0100

    Basic version of form to register done

--- client/src/Screens/Register.jsx
+++ client/src/Screens/Register.jsx
@@ -5,7 +5,42 @@ const Register = () => {
       <div className='col-8'>
         <BgImage />
       </div>
-      <div className='col-4'></div>
+      <div className='col-4'>
+        <div className='acccount'>
+          <div className='acccount__section'>
+            <form>
+              <div className='group'>
+                <input
+                  type='text'
+                  className='group__control'
+                  placeholder='Enter your name'
+                />
+              </div>
+              <div className='group'>
+                <input
+                  type='email'
+                  className='group__control'
+                  placeholder='Enter your email'
+                />
+              </div>
+              <div className='group'>
+                <input
+                  type='password'
+                  className='group__control'
+                  placeholder='Enter your password'
+                />
+              </div>
+              <div className='group'>
+                <input
+                  type='submit'
+                  className='btn btn-default btn-block'
+                  value='Register'
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
