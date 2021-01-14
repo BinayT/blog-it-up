2e5176d756de9fcb52c20eb6b3ffa370373a9b29
Binay <binayprogramming@gmail.com>
2021-01-14 14:56:51 +0100

    Register page started, showing the image according to the navbar position, looking good

--- client/src/Screens/Register.jsx
+++ client/src/Screens/Register.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register page</h1>
+    <div className='row mt-80'>
+      <div className='col-8'>
+        <div className='bgImage'></div>
+      </div>
+      <div className='col-4'></div>
     </div>
   );
 };
