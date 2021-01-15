704df29245c6bf387987b064a6dc0df69958ce21
Binay <binayprogramming@gmail.com>
2021-01-15 16:07:00 +0100

    Reducer and actions naming convention implemented

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import authReducer from './reducers/AuthReducer';
+import authReducer from './reducers/authReducers';
 
 const rootReducers = combineReducers({ auth: authReducer });
 
