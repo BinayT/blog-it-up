bcbc0c623127957bec0e0028de884beef5d3fdf6
Binay <binayprogramming@gmail.com>
2021-01-17 15:00:46 +0100

    Hiding Login and Register button from the navbar if user exists and replacing it with a 'Hi, {username}'

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <nav className='navbar'>
       <div className='container'>
@@ -10,14 +12,22 @@ const Navbar = () => {
               <img src='/images/navbar-logo.png' alt='' />
             </Link>
           </div>
-          <div className='navbar__right'>
-            <li>
-              <Link to='/login'>Login</Link>
-            </li>
-            <li>
-              <Link to='/register'>Register</Link>
-            </li>
-          </div>
+          {user ? (
+            <div className='navbar__right'>
+              <li>
+                <Link to='/dashboard'>Hi, {user.name}</Link>
+              </li>
+            </div>
+          ) : (
+            <div className='navbar__right'>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+              <li>
+                <Link to='/register'>Register</Link>
+              </li>
+            </div>
+          )}
         </div>
       </div>
     </nav>
