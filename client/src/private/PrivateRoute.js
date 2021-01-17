b404084bc43bf17629c590c056b2026350026533
Binay <binayprogramming@gmail.com>
2021-01-17 14:43:24 +0100

    Logic made to when to redirect and where if there's no user in our localstorage

--- client/src/private/PrivateRoute.js
+++ client/src/private/PrivateRoute.js
@@ -1,6 +1,13 @@
 import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 
-const PrivateRoute = (props) => {};
+const PrivateRoute = ({ path, exact, component }) => {
+  const { user } = useSelector((state) => state.auth);
+  return user ? (
+    <Route path={path} exact={exact} component={component} />
+  ) : (
+    <Redirect to='/login' />
+  );
+};
 
 export default PrivateRoute;
