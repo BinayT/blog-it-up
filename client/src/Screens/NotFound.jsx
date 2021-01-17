1f12020da359234a5bb0ea0349b564934f57b42d
Binay <binayprogramming@gmail.com>
2021-01-17 15:54:09 +0100

    Styling the not found page

--- client/src/Screens/NotFound.jsx
+++ client/src/Screens/NotFound.jsx
@@ -2,14 +2,14 @@ import Helmet from '../components/Helmet';
 
 const NotFound = () => {
   return (
-    <div className='notFound dashboard'>
+    <div className='notFound'>
       <Helmet
         title={`404 Not Found`}
         content='The page searched in Blog It Up website is not found.'
       />
       <div className='notFound__container'>
         <h1 className='notFound__container__h1'>404</h1>
-        <p className='notFound__container__h1'>
+        <p className='notFound__container__p'>
           Opps! The page could not be found
         </p>
       </div>
