ce5d52f259249d7a34d20360a3827388a95fd8c5
Binay <binayprogramming@gmail.com>
2021-01-17 16:49:04 +0100

    Login user actions created

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -4,6 +4,9 @@ import {
   REGISTRATION_REQUEST,
   REGISTRATION_SUCCESS,
   REGISTRATION_ERROR,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
   SET_TOKEN,
 } from '../constants/authConstants';
 
@@ -30,4 +33,26 @@ const userRegister = (info) => async (dispatch) => {
   }
 };
 
-export { userRegister };
+const loginUser = (info) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  dispatch({ type: LOGIN_REQUEST });
+  try {
+    const { data } = await axios.post('/login', info, config);
+    dispatch({ type: LOGIN_SUCCESS });
+
+    localStorage.setItem('jwtToken', data.token);
+
+    //We get token before the browser refreshes.
+    dispatch({ type: SET_TOKEN, payload: data.token });
+  } catch (error) {
+    dispatch({ type: LOGIN_ERROR, payload: error.response.data.errors });
+    console.log(error.response.data.errors);
+  }
+};
+
+export { userRegister, loginUser };
