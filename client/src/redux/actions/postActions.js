94ee0853cd7a7bfcd68e659e44304c9b0626a3b4
Binay <binayprogramming@gmail.com>
2021-01-21 17:51:51 +0100

    basic skeleton made for post actions

---
+++ client/src/redux/actions/postActions.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import {
+  CREATE_POST_REQUEST,
+  CREATE_POST_SUCCESS,
+  CREATE_POST_ERROR,
+} from '../constants/postConstants';
+
+export const createPost = (postData) => async (dispatch) => {};
