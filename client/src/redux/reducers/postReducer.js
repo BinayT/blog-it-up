cec6d93ab9bdd4cfc41c138c1078b9555b0a6962
Binay <binayprogramming@gmail.com>
2021-01-21 20:31:03 +0100

    Post reducer done, checking if post is valid or not and sending errors otherwise, errors being displayed on global store

--- client/src/redux/reducers/postReducer.js
+++ client/src/redux/reducers/postReducer.js
@@ -1,10 +1,25 @@
+import {
+  CREATE_POST_REQUEST,
+  CREATE_POST_SUCCESS,
+  CREATE_POST_ERROR,
+} from '../constants/postConstants';
+
 const initialState = {
   loading: false,
   createErrors: [],
 };
 
 const postReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case CREATE_POST_REQUEST:
+      return { ...state, loading: true };
+    case CREATE_POST_SUCCESS:
+      return { ...state, loading: false };
+    case CREATE_POST_ERROR:
+      return { ...state, loading: false, createErrors: action.payload };
+    default:
+      return state;
+  }
 };
 
 export { postReducer };
