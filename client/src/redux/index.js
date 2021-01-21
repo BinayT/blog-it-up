ea3ceda406e7211e10325b2325ebb3777e2a7e02
Binay <binayprogramming@gmail.com>
2021-01-21 17:47:47 +0100

    Imported the reducer to our store + changed authReducer's naming convention to similar one as of post reducer

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import { authReducer, loginReducer } from './reducers/authReducers';
+import { authReducer, loginReducer } from './reducers/authReducer';
+import { postReducer } from './reducers/postReducer';
 
 const rootReducers = combineReducers({
   auth: authReducer,
   login: loginReducer,
+  post: postReducer,
 });
 
 const middlewares = [thunk];
