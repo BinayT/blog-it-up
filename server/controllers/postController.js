1d51eb21c987e252a4ea36b41b05554f2f324ea4
Binay <binayprogramming@gmail.com>
2021-01-25 14:55:00 +0100

    Got rid of bug that was triggering errors if we added right type of extension too

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,5 @@
 import formidable from 'formidable';
+import { v4 as uuidv4 } from 'uuid';
 
 import { customErrors } from '../utils/customErrorMsg.js';
 
@@ -13,16 +14,19 @@ const createPost = (req, res) => {
       errors.push({ msg: 'Image is required' });
     } else {
       const { type } = files.image;
-      const splitExtension = type.split('/')[1];
-
+      const splitExtension = type.split('/')[1].toLowerCase();
+      console.log(splitExtension);
       if (
-        splitExtension !== 'jpeg' ||
-        splitExtension !== 'jpg' ||
+        splitExtension !== 'jpeg' &&
+        splitExtension !== 'jpg' &&
         splitExtension !== 'png'
       ) {
         errors.push({
           msg: `".${splitExtension}" isn't a valid image extension. Please only use .jpg, .jpeg or .png extensions.`,
         });
+      } else {
+        files.image.name = `${uuidv4()}.${splitExtension}`;
+        console.log(files.image.name);
       }
     }
     errors.length !== 0 && res.status(400).json({ errors, files });
